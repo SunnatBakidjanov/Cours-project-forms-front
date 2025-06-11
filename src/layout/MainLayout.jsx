bf14a8f3cdf13efe2adf175066145f8e8b7e1f7f
SunnatBakidjanov <sunnatbackidjanov@gmail.com>
2025-06-11 10:52:39 +0500

    Развертываение приложения

--- src/layout/MainLayout.jsx
+++
@@ -1,7 +0,0 @@
-export const MainLayout = ({ children }) => {
-	return (
-		<>
-			<main>{children}</main>
-		</>
-	);
-};
--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import { App } from './App/App';
-
-const rootElement = document.getElementById('root');
-const createReactRoot = createRoot(rootElement);
-
-createReactRoot.render(<App />);
