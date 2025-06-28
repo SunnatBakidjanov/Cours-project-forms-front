f4004ff5f4d6c85a965cebfa39e4f1641cc9a2fa
SunnatBakidjanov <sunnatbackidjanov@gmail.com>
2025-06-28 23:01:57 +0500

    Добавлен навигате в login
    -- Добавлен navigate в login на кнопку войти

--- src/components/header/UI/login-profile/LoginProfile.jsx
+++ src/components/header/UI/login-profile/LoginProfile.jsx
@@ -1,12 +1,14 @@
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import { Profile } from '../profile/Profile';
 import { LoginLogoutButton } from '../loginBtn/LoginLogoutButton';
 
 export const LoginProfile = () => {
 	const accessToken = useSelector(state => state.auth.accessToken);
+	const navigate = useNavigate();
 	const { t } = useTranslation();
 
-	return accessToken ? <Profile /> : <LoginLogoutButton text={t('header.loginBtn')} />;
+	return accessToken ? <Profile /> : <LoginLogoutButton text={t('header.loginBtn')} onClick={navigate('/login')} />;
 };
